a4850911bc61c3236147879a53b78e2416a12ee4
Ender-osur <ender.ga10@gmail.com>
2025-03-29 11:41:11 -0500

    update

---
+++ src/app/app.routes.server.ts
@@ -0,0 +1,25 @@
+
+import { RenderMode, ServerRoute } from '@angular/ssr';
+
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'dash',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Client,
+  },
+];
